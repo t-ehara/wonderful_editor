6baf1b6e99aa719c45ccd66d593cdfbde2cfc40c
江原巧 <e.t.miiii228@gmail.com>
2021-02-13 16:26:58 +0900

    修正４

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,5 +64,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  confing.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
