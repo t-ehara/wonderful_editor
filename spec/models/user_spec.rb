ffba93db93717398ed03eab4b77593c5542394ad
江原巧 <e.t.miiii228@gmail.com>
2021-02-15 02:14:09 +0900

    rubocopによる修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   context "同じ名前のuserが存在していない時" do
     let(:user) { build(:user) }
     it "userが作られる" do
-      user.save
+      user.save!
       expect(user).to be_valid
     end
   end
 
   context "同じ名前のuserが存在する時" do
+    let(:user) { build(:user, name: "t-ehara") }
     it "user作成に失敗する" do
-      user = create(:user, name: "t-ehara")
-      user2 = build(:user, name: "t-ehara")
-      expect(user2).to be_invalid
+      user.save!
+      user = build(:user, name: "t-ehara")
+      expect(user).to be_invalid
     end
   end
 
   context "nameを指定している時" do
     let(:user) { build(:user, name: "t-ehara") }
     it "ユーザーが作られる" do
-      user.save
+      user.save!
       expect(user).to be_valid
     end
   end
@@ -28,15 +29,15 @@ RSpec.describe User, type: :model do
   context "nameを指定していない時" do
     let(:user) { build(:user, name: nil) }
     it "user作成に失敗する" do
-      user.save
+      user.save!
       expect(user).to be_invalid
     end
   end
 
   context "nameが30文字を超えた時" do
-    let(:user) { build(:user, name: "a"*31) }
+    let(:user) { build(:user, name: "a" * 31) }
     it "ユーザー作成に失敗する" do
-      user.save
+      user.save!
       expect(user).to be_invalid
     end
   end
@@ -44,7 +45,7 @@ RSpec.describe User, type: :model do
   context "同じemailが存在しない時" do
     let(:user) { build(:user, email: "t-ehara@example.com") }
     it "userが作成される" do
-      user.save
+      user.save!
       expect(user).to be_valid
     end
   end
@@ -52,7 +53,7 @@ RSpec.describe User, type: :model do
   context "同じemailが存在する時" do
     let(:user) { build(:user, email: "t-ehara@example.com") }
     it "user作成に失敗する" do
-      user.save
+      user.save!
       user2 = build(:user, email: "t-ehara@example.com")
       expect(user2).to be_invalid
     end
@@ -61,7 +62,7 @@ RSpec.describe User, type: :model do
   context "emailが指定されていない時" do
     let(:user) { build(:user, email: nil) }
     it "user作成に失敗する" do
-      user.save
+      user.save!
       expect(user).to be_invalid
     end
   end
@@ -69,10 +70,8 @@ RSpec.describe User, type: :model do
   context "passwordを指定しない時" do
     let(:user) { build(:user, password: nil) }
     it "user作成に失敗する" do
-      user.save
+      user.save!
       expect(user).to be_invalid
     end
   end
-
-
 end
