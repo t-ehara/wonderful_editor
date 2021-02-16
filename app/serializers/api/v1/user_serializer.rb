ab951a2dcfd93d0d999dc04c2cc569c119fb407e
江原巧 <e.t.miiii228@gmail.com>
2021-02-16 23:06:11 +0900

    api_v1_articlesのrequest_specテスト実装

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :id, :name, :email
   has_many :articles
 end
--- spec/requests/api/v1/articles_request_spec.rb
+++ spec/requests/api/v1/articles_request_spec.rb
@@ -1,7 +1,22 @@
-# rubocop:disable all
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Articles", type: :request do
 
+  describe "GET/api/v1/articles" do
+    subject { get(api_v1_articles_path) }
+    let!(:article1) { create(:article, updated_at: 1.days.ago) }
+    let!(:article2) { create(:article, updated_at: 2.days.ago) }
+    let!(:article3) { create(:article, updated_at: 3.days.ago) }
+
+    fit "ユーザーの一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      binding.pry
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+      expect(response).to have_http_status(200)
+      expect(res.map { |d| d["id"] }).to eq [article1.id, article2.id, article3.id]
+    end
+  end
+
 end
-# rubocop:enable all
